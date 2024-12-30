3d12f51248523d3510cb2585605110f4d8644d7b
Nisha <Nisha@DESKTOP-HDO2FJ2>
2024-12-30 03:23:54 -0800

    Update half_Pyramid2

--- Patterns/src/JavaPatterns/half_Pyramid2.java
+++ Patterns/src/JavaPatterns/half_Pyramid2.java
@@ -3,7 +3,8 @@ package JavaPatterns;
 public class half_Pyramid2 {
 
 	public static void main(String[] args) {
-		for(int i=4; i>=1; i--) {
+		int row =4;
+		for(int i=row; i>=1; i--) {
 			for(int j=1; j<=i; j++) {
 				System.out.print("*");
 			}System.out.println();
