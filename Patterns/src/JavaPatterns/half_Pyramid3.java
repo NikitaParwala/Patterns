01721d47c738aa7725654fc2c09ddbf8008b645f
Nisha <Nisha@DESKTOP-HDO2FJ2>
2024-12-30 04:25:25 -0800

    update half_Pyramid

--- Patterns/src/JavaPatterns/half_Pyramid3.java
+++ Patterns/src/JavaPatterns/half_Pyramid3.java
@@ -1,22 +1,29 @@
 package JavaPatterns;
+import java.util.*;
 
 public class half_Pyramid3 {
 
 	public static void main(String[] args) {
-		
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number of row u wanted to print pattern : ");
+		int row = sc.nextInt();
+		for(int i=1;i<=row;i++) {
+			for(int k=row; k>i; k--) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
-
 }
-
 
 /*
 
-    *
-   **
-  ***
- ****
-*****
+  *
+ **
+***
 
 
-*/ 
+*/
