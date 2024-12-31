6205dd3bc8bc5a741a04e67fb552823fa452fdbc
Nisha <Nisha@DESKTOP-HDO2FJ2>
2024-12-30 21:10:25 -0800

    Added butterfly_Pattern

---
+++ Patterns/src/JavaPatterns/butterfly_Pattern.java
@@ -0,0 +1,23 @@
+package JavaPatterns;
+
+public class butterfly_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+/*
+
+*	 *
+**	**
+******
+**	**
+*	 *
+
+
+*/
